57b13c88b062c3c7b8f8b82d347eb0dc070bb71b
Sangbin Lee <leesangbin@outlook.com>
2020-06-04 23:41:23 +0200

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {useTranslation} from "react-i18next";
+import {Link} from "gatsby";
+import {Button, Divider, Typography} from "@material-ui/core";
+
+const NotFound = () => {
+    const {t, ready} = useTranslation();
+
+    return ready ? <>
+        <Typography variant={"h3"}>{t("page not found")}</Typography>
+        <Divider/>
+        <Button component={Link} to={"/"}>{t("go to home")}</Button>
+    </> : <></>
+}
+
+export default NotFound;
